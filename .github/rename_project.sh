7ef8128c866271d8698703517e9d6b0aeb031523
jdkoen <jdkoen@users.noreply.github.com>
2022-08-13 16:01:44 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="jdkoen"
+original_name="koenlab_pytools"
+original_urlname="koenlab-pytools"
+original_description="Awesome koenlab_pytools created by jdkoen"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv koenlab_pytools $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from koenlab_pytools.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "koenlab_pytools"
